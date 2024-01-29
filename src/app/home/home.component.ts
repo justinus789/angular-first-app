aa7e64b93da733e228fda39e9ae5ab01f19a8b13
Justinus Andreas <firemage0852@gmail.com>
2024-01-29 17:31:11 +0700

    template variable & search function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,19 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          (click)="filterResults(filter.value)"
+          class="primary"
+          type="button"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       />
     </section>
@@ -29,8 +35,21 @@ export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((item) =>
+      item?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
